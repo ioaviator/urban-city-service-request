9ed970d4665bde68fe4f64f29373f885423dc49d
ioaviator <kingsolomonifeanyi@gmail.com>
2026-05-30 13:01:30 +0100

    chore(terraform): manage the backend state file using terraform

--- terraform/tf-backend/main.tf
+++ terraform/tf-backend/main.tf
@@ -7,9 +7,24 @@ resource "azurerm_resource_group" "tfbackend" {
 resource "azurerm_storage_account" "urbancitytfstorage" {
   name                     = "urbancitytfstatebackend"
   resource_group_name      = azurerm_resource_group.tfbackend.name
-  location                 = "East US"
+  location                 = azurerm_resource_group.tfbackend.location
   account_tier             = "Standard"
   account_replication_type = "GRS"
+
+  blob_properties {
+    # enable blob versioning
+    versioning_enabled = true
+
+    # enable soft delete for blobs
+    delete_retention_policy {
+      days = 15
+    }
+
+    #  enable soft delete for parent container
+    container_delete_retention_policy {
+      days = 15
+    }
+  }
 }
 
 resource "azurerm_storage_container" "tfstatecontainer" {
