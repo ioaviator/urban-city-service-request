5270eb545824209921a838416ebd6da395e84e98
ioaviator <kingsolomonifeanyi@gmail.com>
2026-05-30 14:18:31 +0100

    chore(terraform): setup azure provider and save the state file to azure backend

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,4 +1,23 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "=4.1.0"
+    }
+  }
+
+  backend "azurerm" {
+    resource_group_name  = "tf-backend"
+    storage_account_name = "urbancitytfstatebackend"
+    container_name       = "tfstate"
+    key                  = "terraform.tfstate"
+  }
+}
+
 # Configure the Microsoft Azure Provider
 provider "azurerm" {
   features {}
+  
+  subscription_id = file("credentials.txt")
+
 }
