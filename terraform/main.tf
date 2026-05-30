0bbb5b6d2b955a8ada6b19f804d2e0fe33bdda44
ioaviator <kingsolomonifeanyi@gmail.com>
2026-05-30 14:16:17 +0100

    chore(terraform): provision storage account and blob containers

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,45 +1,27 @@
 
-resource "azurerm_resource_group" "urbancityresource" {
+resource "azurerm_resource_group" "urbancityrg" {
   name     = var.resource_group_name
   location = var.location
 }
 
-data "azurerm_storage_account" "storageaccountdata" {
-  name                = azurerm_storage_account.urbancitystorage.name
-  resource_group_name = var.resource_group_name
-  depends_on          = [azurerm_storage_account.urbancitystorage]
-}
-
-resource "azurerm_storage_account" "urbancitystorage" {
+resource "azurerm_storage_account" "urbancitystorageacct" {
   name                     = var.storage_account_name
-  resource_group_name      = azurerm_resource_group.urbancityresource.name
+  resource_group_name      = azurerm_resource_group.urbancityrg.name
   location                 = var.location
   account_tier             = "Standard"
   account_replication_type = "GRS"
 }
 
-resource "azurerm_storage_container" "rawstoragecontainer" {
-  name                  = "raw"
-  storage_account_name  = azurerm_storage_account.urbancitystorage.name
+resource "azurerm_storage_container" "bronzecontainer"{
+  name = "bronze"
+  storage_account_name = azurerm_storage_account.urbancitystorageacct.name
   container_access_type = "private"
+  depends_on = [ azurerm_storage_account.urbancitystorageacct ]
 }
 
-
-resource "azurerm_data_factory" "urbancitydatafactory" {
-  name                = "urbancityadf"
-  location            = azurerm_resource_group.urbancityresource.location
-  resource_group_name = azurerm_resource_group.urbancityresource.name
+resource "azurerm_storage_container" "silvercontainer" {
+  name                  = "silver"
+  storage_account_name = azurerm_storage_account.urbancitystorageacct.name
+  container_access_type = "private"
+  depends_on = [ azurerm_storage_account.urbancitystorageacct ]
 }
-
-
-resource "azurerm_data_factory_linked_service_azure_blob_storage" "urbancityblobstoragels" {
-  name              = "urban_city_blob_ls"
-  data_factory_id   = azurerm_data_factory.urbancitydatafactory.id
-  connection_string = data.azurerm_storage_account.storageaccountdata.primary_connection_string
-}
-
-# module "data_factory_blob_storage" {
-#   source = "./data_factory_blob_storage"
-#   data_factory_id = azurerm_data_factory.cdedatafactory.id
-#   linked_service_name = azurerm_data_factory_linked_service_azure_blob_storage.urbancityblobstoragels.name
-# }
