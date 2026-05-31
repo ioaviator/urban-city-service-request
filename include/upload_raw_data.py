07ada406a37a5927d7dd52f28feda7600e2b04d5
ioaviator <kingsolomonifeanyi@gmail.com>
2026-05-31 12:36:14 +0100

    feat(py): upload raw data to storage account

---
+++ include/upload_raw_data.py
@@ -0,0 +1,43 @@
+import os
+from azure.storage.blob import BlobServiceClient
+from dotenv import load_dotenv
+from pathlib import Path
+
+location = Path(__file__).resolve().parent
+project_root = location.parent
+
+load_dotenv()
+
+ACCOUNT_KEY = os.getenv('ACCOUNT_KEY')
+ACCOUNT_NAME = 'urbancitystorageacct'
+CONTAINER_NAME = "bronze"
+LOCAL_FILE_PATH = project_root / "urban_service_requests.csv"
+
+BLOB_NAME = "urban_service_requests.csv"
+
+def upload_data():
+    try:
+        blob_service_client = BlobServiceClient(
+            account_url=f"https://{ACCOUNT_NAME}.blob.core.windows.net",
+            credential=ACCOUNT_KEY
+        )
+       
+        blob_client = blob_service_client.get_blob_client(container=CONTAINER_NAME, blob=BLOB_NAME)
+
+        print(f" Starting upload: {LOCAL_FILE_PATH}...")
+
+        with open(LOCAL_FILE_PATH, "rb") as data:
+            blob_client.upload_blob(
+                data,
+                overwrite=True,
+                # Adjust max_concurrency for even more speed 
+                # (uses multiple threads to upload chunks)
+                max_concurrency=4 
+            )
+
+        print(f"Upload complete! File is now in container: {CONTAINER_NAME}")
+
+    except Exception as e:
+        print(f"Failed to upload: {e}")
+
+upload_data()
