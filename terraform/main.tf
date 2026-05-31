dab3f1e29f52dc8e187a4559ea19e1475235b1bb
ioaviator <kingsolomonifeanyi@gmail.com>
2026-05-31 11:23:40 +0100

    chore(terraform): setup azure postgresdb service

--- terraform/main.tf
+++ terraform/main.tf
@@ -33,6 +33,45 @@ resource "azurerm_storage_container" "silvercontainer" {
 }
 
 
+// postgresql server
+resource "azurerm_postgresql_flexible_server" "urbancityserver" {
+  name                          = var.postgres_server
+  resource_group_name           = var.resource_group_name
+  location                      = var.location
+  version                       = "16"
+  public_network_access_enabled = true
+  administrator_login           = var.db_admin_login
+  administrator_password        = var.db_admin_pass
+  zone                          = "1"
+
+  storage_mb   = 32768
+  storage_tier = "P30"
+
+  sku_name    = "GP_Standard_D4s_v3"
+  create_mode = "Default"
+
+  authentication {
+    password_auth_enabled = true
+  }
+
+  depends_on = [ azurerm_resource_group.urbancityrg ]
+
+}
+
+resource "azurerm_postgresql_flexible_server_database" "urbancitydb" {
+  name      = "urban_city_db"
+  server_id = azurerm_postgresql_flexible_server.urbancityserver.id
+  collation = "en_US.utf8"
+  charset   = "utf8"
+
+  # prevent the possibility of accidental data loss
+  lifecycle {
+    prevent_destroy = false
+  }
+}
+
+
+
 module "data_factory_blob_storage" {
   source = "./data_factory_blob_storage"
   location            = azurerm_resource_group.urbancityrg.location
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -13,3 +13,18 @@ variable "location" {
   default = "East US"
 }
 
+variable "postgres_server" {
+  type = string
+  description = "postgres server name"
+  default = "urban-city-requests"
+}
+
+variable "db_admin_login"{
+  type = string
+  description = "postgres server login username"
+}
+
+variable "db_admin_pass"{
+  type = string
+  description = "postgres server login password"
+}
